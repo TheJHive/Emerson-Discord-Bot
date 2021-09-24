ddaaa96c9979dcefed8b65a6b176b3f8df9aa02c
The J Hive <87556958+TheJHive@users.noreply.github.com>
2021-09-23 17:52:43 -0700

    Fix outdated bits

--- commands/help.js
+++ commands/help.js
@@ -18,12 +18,14 @@ const help = new MessageEmbed()
 		{name: '`/lunchgroups`', value: 'Pulls up a list of Lunch Time Student Group Zoom links.', inline: true},
 		{name: '`/parentportal`', value: 'Pulls up the link to the LAUSD Parent Portal Student Login, where you can get your SSID.', inline: true},
 		{name: '`/daysleft`', value: 'Tells you how many days of school are left.', inline: true},
-		{name: '`/role`', value: 'Gives you certain roles based off of what you use the command with. **Use `/r-help` for a guide to this command!**', inline: true},
+		{name: '`/role`', value: 'Gives you certain roles based off of what you use the command with. **Use `/rhelp` for a guide to this command!**', inline: true},
 		{name: '`/policies`', value: 'Pulls up a link to Emerson\'s Policies and Procedures.', inline: true},
 		{name: '`/calculator`', value: 'Put numbers and an operator with this command and the bot will act as a calculator.'},
 		{name: '`/buttons`', value: 'Makes buttons that do nothing.', inline: true},
+		{name: '`/cat`', value: 'Sends a random cat from https://aws.random.cat/meow (This is mostly an experiment with REST APIs.)'},
 		{name: '`/github`', value: 'Pulls up a link to the public bot GitHub repo.', inline: true},
 		{name: '`/clearconsole`', value: 'Clears the node.js console of whomever is running the bot.\n(Can only be used by Bot Programmers)'},
+		{name: '`/stop`', value: 'Completely stops the bot and takes it offline. (This requires both a special role and a password!)'},
 		{name: '`/invitebot`', value: 'Gives you a link to add this bot to your own server!', inline: true},
 		{name: '`/fart`', value: 'fart', inline: true}
 	);
